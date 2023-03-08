8927aa63e5c25e305cfead99b3071443c117db77
ayaeid <ayaaeid.72@gmail.com>
2023-03-08 16:27:09 +0200

    chanage search icone

--- lib/views/notes_app.dart
+++ lib/views/notes_app.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import 'package:flutter/material.dart';
+
+import '../widget/note_view_boddy.dart';
+
 class NotsView extends StatelessWidget {
   const NotsView ({Key? key}) :super(key:key );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
+
+
