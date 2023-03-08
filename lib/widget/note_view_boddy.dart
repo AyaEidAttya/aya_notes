82fb8a8807e61203d712ab0f8a0282392f7765c8
ayaeid <ayaaeid.72@gmail.com>
2023-03-08 16:55:07 +0200

    creat note_iteam

--- lib/widget/note_view_boddy.dart
+++ lib/widget/note_view_boddy.dart
@@ -15,9 +15,40 @@ class  NotesViewBody extends StatelessWidget {
           height: 50,
         ),
         CoustomAppBar(),
+        NoteIteam(),
       ],
       ),
 
+    );
+  }
+}
+class NoteIteam extends StatelessWidget {
+  const NoteIteam({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:BoxDecoration(
+        color: Colors.red, 
+        borderRadius: BorderRadius.circular(16),
+      
+      ),
+      child:Column (children: [
+        ListTile(
+
+          title:Text('fluttert tips' , style: TextStyle(
+            color:Colors.black54, 
+          ),),
+          subtitle: Text('build your carer with aya eid',style: TextStyle(
+            color:Colors.black54, 
+          ), ),
+          trailing:IconButton(onPressed: () {} ,icon: Icon(Icons.delete),),
+        ),
+
+
+
+      ]),
+      
     );
   }
 }
